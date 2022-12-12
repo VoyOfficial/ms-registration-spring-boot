a6aba29581305f2e73f1422f335e5fb603c2efa5
matheuscarv69 <matheus9126@gmail.com9>
2022-12-11 22:56:11 -0400

    :test_tube: test: creating test cenaries for findUserById of RelationalUserRepositoryTest

--- src/test/java/src/infrastructure/repository/relational/RelationalUserRepositoryTest.java
+++ src/test/java/src/infrastructure/repository/relational/RelationalUserRepositoryTest.java
@@ -5,13 +5,16 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import src.UserDatas;
+import src.domain.entity.User;
 import src.infrastructure.model.UserModel;
 import src.infrastructure.repository.jpa.UserJpaRepository;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -45,6 +48,46 @@ class RelationalUserRepositoryTest {
         assertEquals(userDomain, savedUser);
 
         verify(jpaRepository, times(1)).save(any(UserModel.class));
+
+    }
+
+    @Test
+    @DisplayName("Must to Find an User by CPF")
+    void mustToFindAnUserByCPF() {
+
+        // cenary
+        var userCpf = UserDatas.CPF;
+        var expectedOptionalUser = Optional.of(
+                UserDatas.makeAnUserDomain(UserDatas.ID)
+        );
+
+        when(jpaRepository.findByCpf(userCpf)).thenReturn(Optional.of(
+                        new UserModel(expectedOptionalUser.get())
+                )
+        );
+
+        // action
+        var optionalUser = relationalRepository.findByCpf(userCpf);
+
+        // validation
+        assertEquals(expectedOptionalUser, optionalUser);
+
+    }
+
+    @Test
+    @DisplayName("Don't should to Find an User by CPF")
+    void dontShouldToFindAnUserByCPF() {
+
+        // cenary
+        var userCpf = UserDatas.CPF;
+
+        when(jpaRepository.findByCpf(userCpf)).thenReturn(Optional.empty());
+
+        // action
+        var optionalUser = relationalRepository.findByCpf(userCpf);
+
+        // validation
+        assertTrue(optionalUser.isEmpty());
 
     }
 
