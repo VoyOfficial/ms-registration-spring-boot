1f74b8b7f9d03901781ff0da8fb8012794bf64e2
matheuscarv69 <matheus9126@gmail.com9>
2022-12-18 14:08:33 -0400

    :books: docs: implementing open api

--- src/main/java/src/application/controller/UserController.java
+++ src/main/java/src/application/controller/UserController.java
@@ -1,28 +1,43 @@
 package src.application.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 import src.application.controller.request.UserRequest;
-import src.domain.service.UserRegistryService;
+import src.application.interceptor.StandardError;
 import src.domain.usecase.UserRegistryUseCase;
 
 import javax.validation.Valid;
 
+import static org.springframework.http.HttpStatus.CREATED;
+
+@Tag(name = "User", description = "Endpoint with all operations of User")
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/v1/users")
 public class UserController {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private UserRegistryUseCase service;
 
+    @Operation(summary = "Register a new User")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User Created", content = @Content, headers = @Header(name = "Location", description = "Url to access the created resource")),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(implementation = StandardError.class))),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(implementation = StandardError.class)))
+    })
+    @ResponseStatus(CREATED)
     @PostMapping
     public ResponseEntity<Void> registry(
             @RequestBody @Valid UserRequest request,
--- src/main/java/src/application/interceptor/StandardError.java
+++ src/main/java/src/application/interceptor/StandardError.java
@@ -1,5 +1,6 @@
 package src.application.interceptor;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -11,11 +12,21 @@ import java.util.Map;
 @Builder
 public class StandardError {
 
+    @Schema(example = "2022-12-18T16:43:18.114Z")
     private Instant timestamp;
+
+    @Schema(example = "400")
     private Integer status;
+
+    @Schema(example = "Validation")
     private String error;
+
+    @Schema(example = "Validation Error")
     private String message;
+
+    @Schema(example = "/api/registration/v1/users")
     private String path;
+
     private Map<String, String> errors = new HashMap<>();
 
 }
