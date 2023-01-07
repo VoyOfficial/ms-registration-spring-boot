6bba72fe23992ab049fbb9170e09bde9f1e59be8
matheuscarv69 <matheus9126@gmail.com9>
2023-01-07 19:36:58 -0400

    :test_tube: test: testing RelationalUserRepository's findById method

--- src/test/java/src/infrastructure/repository/relational/RelationalUserRepositoryTest.java
+++ src/test/java/src/infrastructure/repository/relational/RelationalUserRepositoryTest.java
@@ -109,4 +109,44 @@ class RelationalUserRepositoryTest {
 
     }
 
+    @Test
+    @DisplayName("Must to Find an User by ID")
+    void mustToFindAnUserByID() {
+
+        // cenary
+        var userId = UserDatas.ID;
+        var expectedOptionalUser = Optional.of(
+                UserDatas.makeAnUserDomain(UserDatas.ID)
+        );
+
+        when(jpaRepository.findById(userId)).thenReturn(Optional.of(
+                        new UserModel(expectedOptionalUser.get())
+                )
+        );
+
+        // action
+        var optionalUser = relationalRepository.findById(userId);
+
+        // validation
+        assertEquals(expectedOptionalUser, optionalUser);
+
+    }
+
+    @Test
+    @DisplayName("Don't should to Find an User by ID")
+    void dontShouldToFindAnUserByID() {
+
+        // cenary
+        var userId = UserDatas.ID;
+
+        when(jpaRepository.findById(userId)).thenReturn(Optional.empty());
+
+        // action
+        var optionalUser = relationalRepository.findById(userId);
+
+        // validation
+        assertTrue(optionalUser.isEmpty());
+
+    }
+
 }
