8c6fd7066090137c4a2920d2e83d77eac34e9495
matheuscarv69 <matheus9126@gmail.com9>
2023-04-17 19:43:16 -0400

    :recycle: refactor: adding response type in LocationAPIPort and GooglePlacesAPIAdapter

--- src/main/java/src/domain/ports/LocationApiPort.java
+++ src/main/java/src/domain/ports/LocationApiPort.java
@@ -2,11 +2,12 @@ package src.domain.ports;
 
 import com.google.maps.errors.ApiException;
 import com.google.maps.model.LatLng;
+import com.google.maps.model.PlacesSearchResponse;
 
 import java.io.IOException;
 
 public interface LocationApiPort {
 
-    public Object getNearbyPlaces(LatLng location) throws IOException, InterruptedException, ApiException;
+    PlacesSearchResponse getNearbyPlaces(LatLng location, Integer radius) throws IOException, InterruptedException, ApiException;
 
 }
