3668be4226af63306c1879c3cb890e9743071f66
matheuscarv69 <matheus9126@gmail.com9>
2023-04-17 19:43:16 -0400

    :recycle: refactor: adding response type in LocationAPIPort and GooglePlacesAPIAdapter

--- src/main/java/src/application/providers/adapters/GooglePlacesAPIAdapter.java
+++ src/main/java/src/application/providers/adapters/GooglePlacesAPIAdapter.java
@@ -4,6 +4,7 @@ import com.google.maps.GeoApiContext;
 import com.google.maps.PlacesApi;
 import com.google.maps.errors.ApiException;
 import com.google.maps.model.LatLng;
+import com.google.maps.model.PlacesSearchResponse;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,28 +24,29 @@ public class GooglePlacesAPIAdapter implements LocationApiPort {
     String apiKey;
 
     @Override
-    public Object getNearbyPlaces(LatLng location) throws IOException, InterruptedException, ApiException {
+    public PlacesSearchResponse getNearbyPlaces(
+            LatLng location,
+            Integer radius
+    ) throws IOException, InterruptedException, ApiException {
 
-        logger.info("GOOGLE PLACES API ADAPTER - STARTING NEARBY SEARCH - LOCATION: {}", location);
+        logger.info("GOOGLE PLACES API ADAPTER - STARTING NEARBY SEARCH - Location: {}", location);
 
         GeoApiContext context = new GeoApiContext
                 .Builder()
                 .apiKey(apiKey)
                 .build();
 
-        var placesResponse = PlacesApi
+        PlacesSearchResponse placesSearchResponse = PlacesApi
                 .nearbySearchQuery(context, location)
-                .radius(5000)
+                .radius(radius)
                 .language("en")
                 .await();
 
-        logger.info("GOOGLE PLACES API ADAPTER - FINISH NEARBY SEARCH - LOCATION: {}", location);
+        logger.info("GOOGLE PLACES API ADAPTER - FINISH NEARBY SEARCH - Places: {}", placesSearchResponse);
 
         context.shutdown();
 
-        System.out.println(placesResponse);
-
-        return null;
+        return placesSearchResponse;
 
     }
 
