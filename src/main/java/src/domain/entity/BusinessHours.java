3f9e25922664a394bb74e2c34f874ed074af93a8
matheuscarv69 <matheus9126@gmail.com9>
2023-09-10 15:21:19 -0400

    :sparkles: feat: adding BusinessHours weekday and interval params

--- src/main/java/src/domain/entity/BusinessHours.java
+++ src/main/java/src/domain/entity/BusinessHours.java
@@ -1,4 +1,13 @@
 package src.domain.entity;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class BusinessHours {
+
+    private String weekday;
+    private Interval interval;
+
 }
