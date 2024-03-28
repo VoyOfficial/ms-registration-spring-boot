bd95963a4253e121fecdc3dd9408b130871e19f2
matheuscarv69 <matheus9126@gmail.com9>
2024-03-27 20:13:51 -0400

    :test_tube: test: adjusting scenarios to PlaceRegistryServiceTest

--- src/test/java/src/domain/service/PlaceRegistryServiceTest.java
+++ src/test/java/src/domain/service/PlaceRegistryServiceTest.java
@@ -1,119 +1,141 @@
 package src.domain.service;
 
-import com.google.maps.model.Geometry;
-import com.google.maps.model.LatLng;
-import com.google.maps.model.PlaceDetails;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import src.domain.entity.Place;
+import src.domain.entity.PlaceDetails;
+import src.domain.exception.CityDifferentPlaceRecommendationException;
 import src.domain.exception.PlaceAlreadyExistsException;
+import src.domain.ports.GooglePlacesPort;
 import src.domain.repository.PlaceRepository;
-import src.infrastructure.agents.PlacesApiClient;
 
-import java.net.URL;
-import java.time.LocalDate;
-import java.util.Date;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 public class PlaceRegistryServiceTest {
 
     @Mock
     private PlaceRepository repository;
 
     @Mock
-    private PlacesApiClient placesApiClient;
+    private GooglePlacesPort googlePlacesPort;
 
     @InjectMocks
     private PlaceRegistryService placeRegistryService;
 
-    @BeforeEach
-    public void setUp() {
-        MockitoAnnotations.openMocks(this);
+    @Test
+    @DisplayName("Must to Registry an Recommended Place")
+    void mustToRegistryAnRecommendPlace() {
+
+        // scenario
+        var expectedRecommendedPlaceId = 1L;
+        Place placeDomain = Place
+                .builder()
+                .id(expectedRecommendedPlaceId)
+                .name("Test Recommended Place")
+                .city("Gramado")
+                .ranking(2)
+                .build();
+
+        PlaceDetails recommendedPlaceDetails = createRecommendedPlaceDetails();
+
+        when(googlePlacesPort.getPlaceFromText(placeDomain.getName(), placeDomain.getCity())).thenReturn(recommendedPlaceDetails);
+        when(repository.findPlaceByGooglePlaceId(recommendedPlaceDetails.getGooglePlaceId())).thenReturn(Optional.empty());
+
+        when(repository.savePlace(any(Place.class))).thenReturn(placeDomain);
+
+        // action
+        Long recommendedPlaceId = placeRegistryService.registry(placeDomain);
+
+        // validation
+        assertEquals(expectedRecommendedPlaceId, recommendedPlaceId);
+
+        verify(repository, times(1)).findPlaceByGooglePlaceId(anyString());
+        verify(repository, times(1)).savePlace(any(Place.class));
+
     }
 
     @Test
-    void registry_NewPlace_ShouldSaveSuccessfully() {
-        // Arrange
-        Place place = new Place();
-        place.setId(123L);
-        place.setName("Test Place");
-        place.setCity("Test City");
-        place.setStartRecommendation(new Date());
+    @DisplayName("Don't should to Registry an Recommended Place when this already exists in database")
+    void dontShouldToRegistryAnRecommendedPlaceWhenThisAlreadyExistsInDatabase() {
 
-        PlaceDetails placeDetails = placeDetailsGenerator();
+        // scenario
+        var expectedExceptionMessage = "place.already.exists.default.message";
+        var expectedRecommendedPlaceId = 1L;
+        Place placeDomain = Place
+                .builder()
+                .id(expectedRecommendedPlaceId)
+                .name("Test Recommended Place")
+                .city("Gramado")
+                .ranking(2)
+                .build();
 
-        when(placesApiClient.getPlaceFromText("Test Place", "Test City")).thenReturn(placeDetails);
-        when(repository.findByGooglePlaceId(anyString())).thenReturn(false);
-        when(repository.savePlace(any())).thenReturn(place);
+        PlaceDetails recommendedPlaceDetails = createRecommendedPlaceDetails();
 
-        // Act
-        Long savedId = placeRegistryService.registry(place);
+        when(googlePlacesPort.getPlaceFromText(placeDomain.getName(), placeDomain.getCity())).thenReturn(recommendedPlaceDetails);
+        when(repository.findPlaceByGooglePlaceId(recommendedPlaceDetails.getGooglePlaceId())).thenReturn(Optional.of(placeDomain));
 
-        // Assert
-        // Verify that the repository save method was called once
-        verify(repository, times(1)).savePlace(any());
-        // Verify that the returned ID is not null
-        assertNotNull(savedId);
+        // action
+        var raisedException = assertThrows(PlaceAlreadyExistsException.class,
+                () -> placeRegistryService.registry(placeDomain)
+        );
+
+        // validation
+        assertEquals(PlaceAlreadyExistsException.class, raisedException.getClass());
+        assertEquals(expectedExceptionMessage, raisedException.getMessage());
+
     }
 
     @Test
-    void registry_PlaceAlreadyExists_ShouldThrowException() {
-        // Arrange
-        Place place = new Place();
-        place.setId(123L);
-        place.setName("Test Place");
-        place.setCity("Test City");
-        place.setStartRecommendation(new Date());
+    @DisplayName("Don't should to Registry an Recommended Place when this city is different between city received in PlaceDomain")
+    void dontShouldToRegistryAnRecommendedPlaceWhenThisCityIsDifferentBetweenCityReceivedInPlaceDomain() {
 
-        PlaceDetails placeDetails = placeDetailsGenerator();
+        // scenario
+        var expectedExceptionMessage = "place.with.city.different.google.default.message";
+        var expectedRecommendedPlaceId = 1L;
+        Place placeDomain = Place
+                .builder()
+                .id(expectedRecommendedPlaceId)
+                .name("Test Recommended Place")
+                .city("Test City")
+                .ranking(2)
+                .build();
 
-        when(placesApiClient.getPlaceFromText("Test Place", "Test City")).thenReturn(placeDetails);
-        when(repository.findByGooglePlaceId(anyString())).thenReturn(true);
+        PlaceDetails recommendedPlaceDetails = createRecommendedPlaceDetails();
 
-        // Act and Assert
-        // Verify that PlaceAlreadyExistsException is thrown
-        assertThrows(PlaceAlreadyExistsException.class, () -> placeRegistryService.registry(place));
+        when(googlePlacesPort.getPlaceFromText(placeDomain.getName(), placeDomain.getCity())).thenReturn(recommendedPlaceDetails);
+
+        // action
+        var raisedException = assertThrows(CityDifferentPlaceRecommendationException.class,
+                () -> placeRegistryService.registry(placeDomain)
+        );
+
+        // validation
+        assertEquals(CityDifferentPlaceRecommendationException.class, raisedException.getClass());
+        assertEquals(expectedExceptionMessage, raisedException.getMessage());
+
     }
 
-    PlaceDetails placeDetailsGenerator() {
-        PlaceDetails placeDetails = new PlaceDetails();
+    private static PlaceDetails createRecommendedPlaceDetails() {
 
-        Geometry geometry = new Geometry();
-        geometry.location = new LatLng(-29.38101710,-50.87110530);
+        return PlaceDetails
+                .builder()
+                .googlePlaceId("ChIJPQmNhEMyGZURxuHk44vIaIw")
+                .name("Test Recommended Place")
+                .contact("(54) 3286-4040")
+                .address("R. Wilma Dinnebier - Bairro Belverede, Gramado - RS, 95670-192, Brazil")
+                .build();
 
-        placeDetails.name = "Hard Rock Cafe";
-        placeDetails.placeId = "ChIJPQmNhEMyGZURxuHk44vIaIw";
-        placeDetails.formattedAddress = "R. Wilma Dinnebier - Bairro Belverede, Gramado - RS, 95670-192, Brazil";
-        placeDetails.adrAddress = "<span class=\"street-address\">R. Wilma Dinnebier</span> - <span class=\"extended-address\">Bairro Belverede</span>, <span class=\"locality\">Gramado</span> - <span class=\"region\">RS</span>, <span class=\"postal-code\">95670-192</span>, <span class=\"country-name\">Brazil</span>";
-        placeDetails.businessStatus = "OPERATIONAL";
-        placeDetails.curbsidePickup = false;
-        placeDetails.geometry = geometry;
-        placeDetails.vicinity = "Rua Wilma Dinnebier - Bairro Belverede, Gramado";
-        placeDetails.formattedPhoneNumber = "(54) 3286-4040";
-        placeDetails.internationalPhoneNumber = "+55 54 3286-4040";
-        placeDetails.icon = null;
-        placeDetails.openingHours = null;
-        placeDetails.utcOffset = -180;
-        placeDetails.userRatingsTotal = 13611;
-        placeDetails.reservable = true;
-        placeDetails.servesBeer = true;
-        placeDetails.servesBreakfast = false;
-        placeDetails.servesDinner = true;
-        placeDetails.servesLunch = true;
-        placeDetails.servesVegetarianFood = false;
-        placeDetails.servesWine = true;
-        placeDetails.wheelchairAccessibleEntrance = true;
-
-
-        return placeDetails;
     }
 
 }
