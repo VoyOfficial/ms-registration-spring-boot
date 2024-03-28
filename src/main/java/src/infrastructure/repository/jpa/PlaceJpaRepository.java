e5f51f743c33750556fbb4beee97070b2952e3f5
matheuscarv69 <matheus9126@gmail.com9>
2024-03-27 20:51:27 -0400

    :recycle: refactor: removing unused method in PlaceJpaRepository

--- src/main/java/src/infrastructure/repository/jpa/PlaceJpaRepository.java
+++ src/main/java/src/infrastructure/repository/jpa/PlaceJpaRepository.java
@@ -1,7 +1,6 @@
 package src.infrastructure.repository.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import src.infrastructure.model.PlaceModel;
 
 import java.util.List;
@@ -12,9 +11,6 @@ public interface PlaceJpaRepository extends JpaRepository<PlaceModel, Long> {
     Optional<PlaceModel> findById(Long placeId);
 
     Optional<List<PlaceModel>> findByCity(String city);
-
-    @Query("SELECT p FROM place p WHERE p.googlePlaceId = :googlePlaceId")
-    PlaceModel findByGooglePlaceId(String googlePlaceId);
 
     Optional<PlaceModel> findPlaceByGooglePlaceId(String googlePlaceId);
 
